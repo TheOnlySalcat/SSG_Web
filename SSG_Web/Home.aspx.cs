f282bb09e157fa115a8f3e4c8fd455300907cc27
TheOnlySalcat <74712815+TheOnlySalcat@users.noreply.github.com>
2020-11-22 11:06:33 +0000

    MVP+ Movies, Quotes, Characters (Comments tidied)
    Tidied code comments

--- SSG_Web/Home.aspx.cs
+++ SSG_Web/Home.aspx.cs
@@ -15,12 +15,9 @@ namespace SSG_Web
         public string apiBaseUrl = "https://the-one-api.dev/v2";
         private string apiToken = "x4IHDwrEg5b11yXtRQxj";
 
-        //Cache for characters to reduce API queries
-        public List<character> cachedCharacters = new List<character>();
-
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //Could do something with URL params here if integrated to another system
         }
 
         protected void btnSearch_Click(object sender, EventArgs e)
@@ -100,8 +97,6 @@ namespace SSG_Web
         {
             if (lbMoviesResult.SelectedItem != null)
             {
-
-
                 //Call API - Get quotes for Movie
                 var client2 = new RestClient($"{apiBaseUrl}/quote?movie={lbMoviesResult.SelectedValue}");
                 client2.Timeout = -1;
@@ -127,7 +122,6 @@ namespace SSG_Web
 
         protected void lbQuotesResult_SelectedIndexChanged(object sender, EventArgs e)
         {
-
             if (lbQuotesResult.SelectedItem != null)
             {
                 //Call API - Get character of quote
@@ -139,10 +133,65 @@ namespace SSG_Web
 
                 characters charactersList = JsonConvert.DeserializeObject<characters>(response.Content);
 
-                //Put results into listbox
+                //Put results into textbox
                 txtCharactersResult.Text = "";
 
                 txtCharactersResult.Text = charactersList.docs[0].name;
+            }
+        }
+
+        protected void lbCharactersResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //no action
+        }
+
+        protected void btnCacheCharacters_Click(object sender, EventArgs e)
+        {
+            //Call API - Get character by name
+            var client = new RestClient($"{apiBaseUrl}/character?name=/{txtSearchCharacter.Text}/i");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", $"Bearer {apiToken}");
+            IRestResponse response = client.Execute(request);
+
+            characters charactersList = JsonConvert.DeserializeObject<characters>(response.Content);
+
+            //Put results into listbox
+            lbCachedCharacters.Items.Clear();
+
+            foreach (character c in charactersList.docs)
+            {
+                ListItem item = new ListItem();
+                item.Value = c._id;
+                item.Text = c.name;
+                lbCachedCharacters.Items.Add(item);
+            }
+        }
+
+        protected void lbCachedCharacters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbCachedCharacters.SelectedItem != null)
+            {
+                //Call API - Get quotes for Character
+                var client2 = new RestClient($"{apiBaseUrl}/quote?character={lbCachedCharacters.SelectedValue}");
+                client2.Timeout = -1;
+                var request2 = new RestRequest(Method.GET);
+                request2.AddHeader("Authorization", $"Bearer {apiToken}");
+                IRestResponse response2 = client2.Execute(request2);
+
+                quotes quotesList = JsonConvert.DeserializeObject<quotes>(response2.Content);
+
+                //Put results into listbox
+                lbQuotesResult.Items.Clear();
+                txtCharactersResult.Text = lbCachedCharacters.SelectedItem.Text;
+
+                foreach (quote q in quotesList.docs)
+                {
+                    ListItem item = new ListItem();
+                    item.Text = q.dialog;
+                    item.Value = $"{q._id}|{q.character}";
+                    lbQuotesResult.Items.Add(item);
+                }
             }
         }
 
@@ -249,61 +298,5 @@ namespace SSG_Web
 
         #endregion
 
-        protected void lbCharactersResult_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //no action
-        }
-
-        protected void btnCacheCharacters_Click(object sender, EventArgs e)
-        {
-            //Call API - Get character
-            var client = new RestClient($"{apiBaseUrl}/character?name=/{txtSearchCharacter.Text}/i");
-            client.Timeout = -1;
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("Authorization", $"Bearer {apiToken}");
-            IRestResponse response = client.Execute(request);
-
-            characters charactersList = JsonConvert.DeserializeObject<characters>(response.Content);
-
-            //Put results into listbox
-            lbCachedCharacters.Items.Clear();
-
-            foreach (character c in charactersList.docs)
-            {
-                cachedCharacters.Add(c);
-
-                ListItem item = new ListItem();
-                item.Value = c._id;
-                item.Text = c.name;
-                lbCachedCharacters.Items.Add(item);
-            }
-        }
-
-        protected void lbCachedCharacters_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (lbCachedCharacters.SelectedItem != null)
-            {
-                //Call API - Get quotes for Movie
-                var client2 = new RestClient($"{apiBaseUrl}/quote?character={lbCachedCharacters.SelectedValue}");
-                client2.Timeout = -1;
-                var request2 = new RestRequest(Method.GET);
-                request2.AddHeader("Authorization", $"Bearer {apiToken}");
-                IRestResponse response2 = client2.Execute(request2);
-
-                quotes quotesList = JsonConvert.DeserializeObject<quotes>(response2.Content);
-
-                //Put results into listbox
-                lbQuotesResult.Items.Clear();
-                txtCharactersResult.Text = lbCachedCharacters.SelectedItem.Text;
-
-                foreach (quote q in quotesList.docs)
-                {
-                    ListItem item = new ListItem();
-                    item.Text = q.dialog;
-                    item.Value = $"{q._id}|{q.character}";
-                    lbQuotesResult.Items.Add(item);
-                }
-            }
-        }
     }
 }
